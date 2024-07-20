d5bf8c3da23c5895e38aa8d3ce3047bda4d6f8b8
wjztwjzt <123343489+wjztwjzt@users.noreply.github.com>
2024-07-20 18:55:29 +0800

    Update 油价.py

--- 油价.py
+++ 油价.py
@@ -1,4 +1,4 @@
-import raw_main_WXP
+import WXP
 import requests,os,time,json,re
 date = time.strftime(f"%Y年%m月%d日",time.localtime(time.time()))
 
@@ -20,6 +20,6 @@ if result:
     print(f"{date},今日价格为:\n92号汽油为：{result.group(1)}\n95号汽油为：{result.group(2)}\n98号汽油为：{result.group(3)}\n0号柴油为：{result.group(4)}")
     xiao = f"92号汽油为：{result.group(1)}\n95号汽油为：{result.group(2)}\n98号汽油为：{result.group(3)}\n0号柴油为：{result.group(4)}"
     biaoti = f"{date}汽油价格"
-    raw_main_WXP.send(biaoti, xiao)
+    WXP.send(biaoti, xiao)
 else:
     print("未匹配到价格信息")
